81447e8bb5dc13eda2b2c54c68dbd8fae5f48a25
Anthony Moss <asmoss93@gmail.com>
2019-05-08 22:01:17 -0400

    adds react and redux dependencies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { rootReducer } from './actions-reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// =====================================================================================
+// Store
+
+const store = createStore(rootReducer);
+window.store = store;
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
